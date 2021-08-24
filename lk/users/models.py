8b3d02aca931894838781c7cfde499331e42caef
SERP-KZ <G@thic1234>
2021-08-24 12:47:18 +0600

    Lesson 2.1

---
+++ lk/users/models.py
@@ -0,0 +1,17 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    COMPANY = 'COMPANY'
+    PRIVATE_PERSON = 'PRIVATE'
+    COMPANY_PRIVATE = [
+        (COMPANY, 'Компания'),
+        (PRIVATE_PERSON, 'Частное лицо')
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    full_name = models.CharField(verbose_name='Наименование полное', max_length=250, unique=True, null=True, blank=True)
+    status = models.CharField(verbose_name='Юр\Физ лицо', max_length=50, choices=COMPANY_PRIVATE, default=COMPANY)
+
